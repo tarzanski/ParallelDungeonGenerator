d1e24b7d6ad498932c1da5f470ed4a613c5385f0
Aleksander Karol Tarczynski <atarczyn@ghc77.ghc.andrew.cmu.edu>
2022-04-11 12:12:53 -0400

    created main, makefile, and gitignore

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+
+
+
+int main() {
+    
+    // initialize SDL interface if using a GUI
+
+    // set up any timing before calling algorithm functions
+
+    // parse arguments if using a gui
+
+
+}
